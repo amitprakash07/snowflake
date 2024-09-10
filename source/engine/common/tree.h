cd0ca4a7c730eb190d4cef3b24fb52c7adcba9d7
Amit Prakash <amit_prakash07@hotmail.com>
2024-09-10 15:36:33 -0400

    Add some basic ds barebone

---
+++ source/engine/common/tree.h
@@ -0,0 +1,49 @@
+#ifndef ENGINE_COMMON_TREE_H
+#define ENGINE_COMMON_TREE_H
+
+#include <cstdint>
+#include <vector>
+#include <string>
+
+enum class TreeTraverseOrder
+{
+    PreOrder,
+    InOrder,
+    PostOrder
+};
+
+template<class DataType, uint8_t Dimension, bool ArrayStorage>
+class TreeNode
+{
+public:
+    TreeNode() : data() {}
+    DataType data;
+protected:
+    uint8_t dimension = Dimension;
+    bool use_array_storage = ArrayStorage;
+};
+
+template<class DataType>
+class BinaryTreeNode : public TreeNode<DataType, 2, false>
+{
+public:
+    TreeNode<DataType, 2, false>* left = nullptr;
+    TreeNode<DataType, 2, false>* right = nullptr;
+};
+
+template<class DataType>
+class BinaryTree
+{
+public:
+    bool Construct(std::vector<DataType> data);
+    std::string Traverse(TreeTraverseOrder order);
+    static void UnitTest();
+private:
+    BinaryTreeNode<DataType>* root = nullptr;
+};
+
+template<class DataType>
+class BinarySearchTree : public BinaryTree<DataType>
+{};
+
+#endif
