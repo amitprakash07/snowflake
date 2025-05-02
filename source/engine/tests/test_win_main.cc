ed633f73e22a7c55609d200044bc755c0b678a75
Amit Prakash <amit_prakash07@hotmail.com>
2025-05-02 17:05:15 -0400

    WIP: Add win32 test project

---
+++ source/engine/tests/test_win_main.cc
@@ -0,0 +1,8 @@
+#include <Windows.h>
+
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+{
+    MessageBox(nullptr, "Testing the Win Main", "Testing", MB_OK);
+}
+
